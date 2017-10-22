9ac6e9d6c7a6ef8f86b88b102e802d78ae0254e2
kirzunoff <kirzunoff.i@gmail.com>
2017-10-23 00:42:07 +0300

    Added function for carousel in block about

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){	
+	
+	
+	$(".about_carouser").owlCarousel({
+		items: 1,
+		itemsDesktopSmall: [979,2],
+		itemsTablet: [768,1],
+		navigation: true,
+		pagination: false,
+		navigationText: true,
+	 });
+	
+	
+	
+});
