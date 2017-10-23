290a96dbbb35c5a0874df3c58bc84960d3485253
kirzunoff <kirzunoff.i@gmail.com>
2017-10-23 18:45:07 +0300

    In file main.js deleted itemsDesktop versions

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,6 @@ $(function(){
 	
 	$(".about_carouser").owlCarousel({
 		items: 1,
-		itemsDesktopSmall: [979,2],
-		itemsTablet: [768,1],
 		navigation: true,
 		pagination: false,
 		navigationText: false,
