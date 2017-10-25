9b7d2710665b0736b48c6b27f7a7ba65d490cb8f
kirzunoff <kirzunoff.i@gmail.com>
2017-10-25 13:28:01 +0300

    in file main.js for carousels added options itemsDesktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,27 @@
 $(function(){	
 	
+	$("#sticky_menu").on("click","a", function (event) {
+		 event.preventDefault();
+		 var id  = $(this).attr('href'),
+			 top = $(id).offset().top-100;
+		 $('body,html').animate({scrollTop: top}, 1500);
+	});
 	
 	$(".about_carouser").owlCarousel({
 		items: 1,
+		itemsDesktop: [1199,1],
+		itemsDesktopSmall: [979,1],
+		itemsTablet: [768,1],
 		navigation: true,
 		pagination: false,
 		navigationText: false,
 	 });
 	
-	$(".trainers_carousel").owlCarousel({
-	  items: 1,
+	$("#trainers_carousel").owlCarousel({
+	  	items: 1,
+		itemsDesktop: [1199,1],
+		itemsDesktopSmall: [979,1],
+		itemsTablet: [768,1],
 		navigation: true,
 		pagination: true,
 		navigationText: false,
